482528d253ba2691571fa716dfe20985d19b8be2
Adam Magaluk <AdamMagaluK@gmail.com>
2014-08-19 14:10:22 -0400

    Scout now inherits from autoscout to pass pin option

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+var util = require('util');
 var AutoScout = require('zetta-auto-scout');
 var PIR = require('./pir_driver');
 
-module.exports = new AutoScout('pir', PIR);
+var BoneScout = module.exports = function(pin) {
+  AutoScout.call(this, 'pir', PIR, pin);
+};
+util.inherits(BoneScout, AutoScout);
+
