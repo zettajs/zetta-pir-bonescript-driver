db13465ea1c3f8d7f0f8bea982041751f95eb569
Matt Dobson <mdobson4@gmail.com>
2014-09-29 11:37:57 -0400

    Updates to PIR driver to use the refactored modules.

--- index.js
+++ index.js
@@ -1,9 +1,66 @@
+var Device = require('zetta-device');
 var util = require('util');
-var AutoScout = require('zetta-auto-scout');
-var PIR = require('./pir_driver');
+var bone = require('bonescript');
 
-var BoneScout = module.exports = function(pin) {
-  AutoScout.call(this, 'pir', PIR, pin);
+var MOTION_THRESHOLD = 500; // ms
+var READ_INTERVAL = 50;
+var PinStates = {
+  0: 'motion',
+  1: 'no-motion'
 };
-util.inherits(BoneScout, AutoScout);
 
+var PIR = module.exports = function(pin) {
+  Device.call(this);
+  this.pin = pin || 'P9_12';
+};
+util.inherits(PIR, Device);
+
+PIR.prototype.init = function(config) {
+  config
+    .state('undetermined')
+    .type('pir')
+    .name('PIR Sensor')
+    .when('undetermined', { allow: ['motion', 'no-motion'] })
+    .when('no-motion', { allow: ['motion'] })
+    .when('motion', { allow: ['no-motion'] })
+    .map('motion', this.motion)
+    .map('no-motion', this.noMotion);
+  
+  var self = this;
+  var timer = null;
+  bone.pinMode(this.pin, bone.INPUT);
+  
+  function checkPin(x) {
+    if (x.err || x.value === undefined) {
+      return;
+    }
+
+    if (self.state === 'undetermined') {
+      self.call(PinStates[x.value], function() {});
+    }
+
+    // state changed
+    if (self.state !== PinStates[x.value] && timer === null) {
+      timer = setTimeout(function() {
+        self.call(PinStates[x.value], function() {});
+      }, MOTION_THRESHOLD);
+    } else if (self.state === PinStates[x.value]){
+      clearTimeout(timer);
+      timer = null;
+    }
+  }
+
+  setInterval(function(){
+    bone.digitalRead(self.pin, checkPin);
+  }, READ_INTERVAL);
+};
+
+PIR.prototype.motion = function(cb) {
+  this.state = 'motion';
+  cb();
+};
+
+PIR.prototype.noMotion = function(cb) {
+  this.state = 'no-motion';
+  cb();
+};
